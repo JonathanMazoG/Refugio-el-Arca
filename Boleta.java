377a93bed6a57e3766ff56008d13904ba141a220
Ange-lical14 <asirley.hoyos@udea.edu.co>
2023-03-22 20:46:53 -0500

    cambios3

--- Boleta.java
+++ Boleta.java
@@ -2,5 +2,12 @@ import java.util.*;
 
 public class Boleta{
     private Animal[] animales;
+
+    public Boleta() {
+    }
+
+    public Boleta(Animal[] animales) {
+        this.animales = animales;
+    }
     
 }
