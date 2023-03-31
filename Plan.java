a72d703353f650b686c379d86ba86435c508290b
Jonathanmg1415 <jonathang1415@gmail.com>
2023-03-31 07:31:05 -0500

    clase abstracta plan

--- Plan.java
+++ Plan.java
@@ -1,56 +1,53 @@
-public class Plan {
+public abstract class Plan {
 
     private int numberOfSlots;
     private String description;
     private String tipoAnimal;
     private double precio;
-   
-    public Plan(int numberOfSlots, String description, String tipoAnimal, double precio) {
+    
+    protected Plan(int numberOfSlots, String description, String tipoAnimal, double precio) {
         this.numberOfSlots = numberOfSlots;
         this.description = description;
         this.tipoAnimal = tipoAnimal;
         this.precio = precio;
     }
     
-    public Plan() {
-    }
-
     public int getNumberOfSlots() {
         return numberOfSlots;
     }
-
+    
     public void setNumberOfSlots(int numberOfSlots) {
         this.numberOfSlots = numberOfSlots;
-        
     }
-
+    
     public String getDescription() {
         return description;
     }
-
+    
     public void setDescription(String description) {
         this.description = description ;
     }
-
+    
     public String gettipoAnimal() {
         return tipoAnimal;
     }
-
+    
     public void settipoAnimal(String tipoAnimal) {
         this.tipoAnimal = tipoAnimal ;
     }
-
+    
     public double getPrecio() {
         return precio;
     }
-
+    
     public void setPrecio(double precio) {
         this.precio = precio;
     }
+    
     @Override
     public String toString() {
         return "Plan{" + "description=" + description + ", numberOfSlots='" + numberOfSlots + ", tipoAnimal='" + tipoAnimal  +'\'' + '}';
     }
-
     
-}
+    public abstract double calcularCosto();
+};
